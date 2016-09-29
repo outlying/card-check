1485f59d8ff7a83312c1c9ea6f85bca02806855c
Iwo Polański <outlying@gmail.com>
2016-09-29 22:37:56 +0200

    Adding more

--- app/src/main/java/com/antyzero/cardcheck/extension/ContextExtension.kt
+++ app/src/main/java/com/antyzero/cardcheck/extension/ContextExtension.kt
@@ -7,6 +7,8 @@ import com.antyzero.cardcheck.CardCheckApplication
 
 fun Context.application() = this.applicationContext as CardCheckApplication
 
+fun Context.toast() = this.toast(this.toString())
+
 fun Context.toast(message: String?) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
 fun Context.toast(@StringRes id: Int) = Toast.makeText(this, id, Toast.LENGTH_SHORT).show()
