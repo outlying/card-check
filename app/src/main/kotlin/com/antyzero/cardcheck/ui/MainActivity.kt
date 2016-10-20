dd64aa85785504c8e1a0cd5a78321744eb706dca
Iwo Polański <outlying@gmail.com>
2016-10-20 15:59:36 +0200

    Revert to 93d971008aa10901d7b9de7d98713238384eedb1

--- app/src/main/kotlin/com/antyzero/cardcheck/ui/MainActivity.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/ui/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.antyzero.cardcheck.ui
 
 import android.os.Bundle
-import android.view.View
+import com.antyzero.cardcheck.CardCheck
 import com.antyzero.cardcheck.R
+import com.antyzero.cardcheck.card.mpk.MpkCard
+import com.antyzero.cardcheck.extension.applicationComponent
+import com.antyzero.cardcheck.extension.toast
+import org.threeten.bp.LocalDate
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import javax.inject.Inject
+
 
 class MainActivity : BaseActivity() {
+
+    @Inject lateinit var cardCheck: CardCheck
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
+        applicationComponent().inject(this)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener(View.OnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() })
+        val card = MpkCard.Kkm(2170708, 20603546690)
+        val localDate = LocalDate.now()
+
+        cardCheck.check(card, localDate)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { this.toast(it.toString()) }
     }
-
 }
