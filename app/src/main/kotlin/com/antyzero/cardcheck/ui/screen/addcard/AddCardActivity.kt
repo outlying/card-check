6b1152a62eb94e11626613432b69246bae0cc1ca
Iwo Polański <outlying@gmail.com>
2016-10-25 10:30:20 +0200

    Working on adding cards

--- app/src/main/kotlin/com/antyzero/cardcheck/ui/screen/addcard/AddCardActivity.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/ui/screen/addcard/AddCardActivity.kt
@@ -1,13 +1,53 @@
 package com.antyzero.cardcheck.ui.screen.addcard
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
 import com.antyzero.cardcheck.R
+import com.antyzero.cardcheck.extension.layoutInflater
 import com.antyzero.cardcheck.ui.BaseActivity
+import kotlinx.android.synthetic.main.activity_card_add.*
 
-class AddCardActivity : BaseActivity() {
+class AddCardActivity : BaseActivity(), AddCardView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_add)
+        spinnerCardProvider.adapter = CardProviderAdapter()
     }
+}
+
+/**
+ * Adapter for spinner
+ */
+class CardProviderAdapter() : BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return parent.context.layoutInflater()
+                .inflate(android.R.layout.simple_list_item_1, parent, false).apply {
+            (findViewById(android.R.id.text1) as TextView).text = getItem(position).name // TODO change name
+        }
+    }
+
+    override fun getItem(position: Int): Provider {
+        return Provider.values()[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return getItem(position).ordinal.toLong()
+    }
+
+    override fun getCount(): Int {
+        return Provider.values().size
+    }
+
+}
+
+/**
+ * List of supported card providers
+ */
+enum class Provider {
+    MPK
 }
--- app/src/main/kotlin/com/antyzero/cardcheck/ui/screen/main/MainActivity.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/ui/screen/main/MainActivity.kt
@@ -1,26 +1,31 @@
 package com.antyzero.cardcheck.ui.screen.main
 
 import android.os.Bundle
+import com.antyzero.cardcheck.R
 import com.antyzero.cardcheck.card.Card
 import com.antyzero.cardcheck.card.CardCheckResult
 import com.antyzero.cardcheck.extension.startActivity
 import com.antyzero.cardcheck.extension.toast
 import com.antyzero.cardcheck.ui.BaseActivity
 import com.antyzero.cardcheck.ui.screen.addcard.AddCardActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-// val card = MpkCard.Kkm(2170708, 20603546690)
 class MainActivity : BaseActivity(), MainView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         val presenter = MainPresenter(this).apply {
             this.attachView(this@MainActivity)
         }
+        button.setOnClickListener { goToAddCardScreen() }
     }
 
     override fun showCards(it: List<Pair<Card, CardCheckResult>>) {
         // TODO card logic
-        toast(it.size.toString())
+        it.first().let {
+            toast("${it.second}")
+        }
     }
 
     override fun goToAddCardScreen() {
