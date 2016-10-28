17c70ab9acf7ddf28f26db56840091948b13a607
Iwo Polański <outlying@gmail.com>
2016-10-28 15:18:18 +0200

    Correct code call

--- app/src/main/kotlin/com/antyzero/cardcheck/extension/ViewExtension.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/extension/ViewExtension.kt
@@ -1,5 +1,6 @@
 package com.antyzero.cardcheck.extension
 
+import android.os.Build
 import android.support.annotation.ColorRes
 import android.view.View
 
@@ -7,4 +8,10 @@ fun View.setVisible(boolean: Boolean) {
     this.visibility = if (boolean) View.VISIBLE else View.GONE
 }
 
-fun View.setBackgroundColorRes(@ColorRes colorRes: Int) = this.setBackgroundColor(resources.getColor(colorRes, null))
+fun View.setBackgroundColorRes(@ColorRes colorRes: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        this.setBackgroundColor(resources.getColor(colorRes, null))
+    }else{
+        this.setBackgroundColor(resources.getColor(colorRes))
+    }
+}
