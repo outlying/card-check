4ab3cb4b7973c478156c03bdf7dbd57c6b04f013
Iwo Polański <outlying@gmail.com>
2016-11-04 16:15:16 +0100

    Periodically displayed card status via notifications

--- app/src/main/kotlin/com/antyzero/cardcheck/job/CardCheckJobService.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/job/CardCheckJobService.kt
@@ -1,11 +1,46 @@
 package com.antyzero.cardcheck.job
 
+import android.util.Log
+import com.antyzero.cardcheck.CardCheck
+import com.antyzero.cardcheck.data.CardTransformer
+import com.antyzero.cardcheck.extension.applicationComponent
+import com.antyzero.cardcheck.job.Jobs.Tags.CARD_CHECK
+import com.antyzero.cardcheck.ui.notification.CardNotification
 import com.firebase.jobdispatcher.JobParameters
 import com.firebase.jobdispatcher.JobService
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import javax.inject.Inject
 
 class CardCheckJobService() : JobService() {
 
+    @Inject lateinit var cardCheck: CardCheck
+    @Inject lateinit var cardNotification: CardNotification
+
+    override fun onCreate() {
+        super.onCreate()
+        applicationComponent().inject(this)
+    }
+
     override fun onStartJob(job: JobParameters): Boolean {
+
+        when (Jobs.Tags.findByName(job.tag)) {
+
+            CARD_CHECK -> {
+                Observable.from(cardCheck.getCards())
+                        .compose(CardTransformer.status(cardCheck))
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                { cardNotification.cardStatus(it.first, it.second) },
+                                {
+                                    Log.w(CardCheckJobService::class.java.simpleName, "Cannot display notification: ${it}")
+                                })
+
+            }
+        }
+
         return false
     }
 
