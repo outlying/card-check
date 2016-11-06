df55a99043feb47d044680602eb7bbf87cbfe745
Iwo Polański <iwo.polanski@schibsted.pl>
2016-11-06 21:14:33 +0100

    Fix lint issues

--- app/src/main/kotlin/com/antyzero/cardcheck/ui/screen/settings/TimePreference.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/ui/screen/settings/TimePreference.kt
@@ -6,10 +6,6 @@ import android.util.AttributeSet
 
 class TimePreference : DialogPreference {
 
-    constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
-
-
 }
