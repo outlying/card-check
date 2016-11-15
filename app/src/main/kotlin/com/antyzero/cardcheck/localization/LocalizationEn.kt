599a8205a1fba9d18af0ca1f45c325cb571f64d6
Iwo Polański <outlying@gmail.com>
2016-11-15 16:06:37 +0100

    Translation for days updated

--- app/src/main/kotlin/com/antyzero/cardcheck/localization/LocalizationEn.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/localization/LocalizationEn.kt
@@ -3,11 +3,19 @@ package com.antyzero.cardcheck.localization
 
 class LocalizationEn : Localization {
 
+    override fun validFor(days: Int): String {
+        return when(days) {
+            1 -> "till tomorrow"
+            0 -> "last day"
+            else -> "${days.inc()} more days"
+        }
+    }
+
     override fun cardLastDays(days: Int): String {
         return when(days) {
             1 -> "Your card will last until tomorrow"
             0 -> "Your card will last until today"
-            else -> "Your card will last $days more days"
+            else -> "Your card will last ${days.inc()} more days"
         }
     }
 }
--- app/src/main/kotlin/com/antyzero/cardcheck/localization/LocalizationPl.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/localization/LocalizationPl.kt
@@ -3,11 +3,21 @@ package com.antyzero.cardcheck.localization
 
 class LocalizationPl : Localization {
 
+    override fun validFor(days: Int): String {
+        return when(days) {
+            1 -> "do jutra"
+            0 -> "ostatni dzień"
+            else -> "${days.inc()} dni"
+        }
+    }
+
     override fun cardLastDays(days: Int): String {
         return when(days) {
             1 -> "Karta będzie ważna do jutra"
             0 -> "Karta będzie ważna do końca dnia"
-            else -> "Karta będzie ważna jeszcze $days dni"
+            else -> "Karta będzie ważna jeszcze ${days.inc()} dni"
         }
     }
+
+
 }
