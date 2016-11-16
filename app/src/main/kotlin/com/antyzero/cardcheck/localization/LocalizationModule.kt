b51e87f3a174d4097e2b93072ff51815f31caf72
Iwo Polański <outlying@gmail.com>
2016-11-16 09:05:56 +0100

    Supress DEPRECATION

--- app/src/main/kotlin/com/antyzero/cardcheck/localization/LocalizationModule.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/localization/LocalizationModule.kt
@@ -25,6 +25,7 @@ class LocalizationModule {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             context.resources.configuration.locales.get(0)
         } else {
+            @Suppress("DEPRECATION")
             context.resources.configuration.locale
         }
     }
