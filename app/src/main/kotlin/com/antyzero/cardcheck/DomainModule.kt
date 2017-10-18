c895852b7001dd449a20396272e34fb65956f1cc
Iwo Polański <outlying@gmail.com>
2017-10-18 10:56:02 +0200

    Hide output of sdkmanager for Travis

--- app/src/main/kotlin/com/antyzero/cardcheck/DomainModule.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/DomainModule.kt
@@ -23,7 +23,7 @@ class DomainModule {
     fun provideFileStorage(context: Context): PersistentStorage {
         return FileStorage("", context.getFileStreamPath("storage").absoluteFile).apply {
             if (BuildConfig.DEBUG) {
-                addCard(MpkCard.Kkm(2170708, 20603546690))
+                //addCard(MpkCard.Kkm(2170708, 20603546690))
             }
         }
     }
