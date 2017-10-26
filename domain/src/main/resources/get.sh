2476d9b5ac7d01c7248c625a69d55c733d50e0cf
Iwo Polański <outlying@gmail.com>
2017-10-26 22:57:09 +0200

    Get test HTML sites

---
+++ domain/src/main/resources/get.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O mpk_accumulate.html http://www.mpk.krakow.pl/pl/sprawdz-waznosc-biletu/index,1.html?identityNumber=2170708&cityCardType=0&dateValidity=2016-10-27&cityCardNumber=20603546690
+wget -O mpk_valid.html http://www.mpk.krakow.pl/pl/sprawdz-waznosc-biletu/index,1.html?identityNumber=2170708&cityCardType=0&dateValidity=2016-7-19&cityCardNumber=20603546690
