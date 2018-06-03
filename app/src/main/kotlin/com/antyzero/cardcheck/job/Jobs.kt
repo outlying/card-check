ba580b9e512b2be0a2a3d3bcddf0fe9b927c7a56
Iwo Polański <outlying@gmail.com>
2018-06-03 22:46:18 +0200

    Working on shared interface scheduler

--- app/src/main/kotlin/com/antyzero/cardcheck/job/Jobs.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/job/Jobs.kt
@@ -1,13 +1,11 @@
 package com.antyzero.cardcheck.job
 
-import com.antyzero.cardcheck.TriggerConfigurator
 import com.antyzero.cardcheck.dsl.extension.TAG
 import com.antyzero.cardcheck.dsl.extension.abs
 import com.antyzero.cardcheck.dsl.extension.betweenWithMidnight
 import com.antyzero.cardcheck.job.Jobs.Tags.CARD_CHECK
 import com.antyzero.cardcheck.logger.Logger
 import com.firebase.jobdispatcher.FirebaseJobDispatcher
-import com.firebase.jobdispatcher.Job
 import com.firebase.jobdispatcher.Lifetime
 import org.threeten.bp.LocalTime
 import org.threeten.bp.temporal.ChronoUnit
@@ -32,7 +30,6 @@ class Jobs(private val dispatcher: FirebaseJobDispatcher) {
             TriggerConfigurator.executionWindow(this, windowStart, windowEnd)
 
         }.build().let {
-
             if (dispatcher.schedule(it) != FirebaseJobDispatcher.SCHEDULE_RESULT_SUCCESS) {
                 Logger.w(TAG, "Unable to schedule card check")
             }
@@ -56,6 +53,4 @@ class Jobs(private val dispatcher: FirebaseJobDispatcher) {
             }
         }
     }
-
-    private fun Job.Builder.setTag(tag: Tags) = this.setTag(tag.name)
 }
--- app/src/main/kotlin/com/antyzero/cardcheck/scheduler/AndroidScheduler.kt
+++ app/src/main/kotlin/com/antyzero/cardcheck/scheduler/AndroidScheduler.kt
@@ -3,7 +3,9 @@ package com.antyzero.cardcheck.scheduler
 
 class AndroidScheduler :Scheduler{
 
-    override fun addSchedule(): Boolean = true
+    override fun addSchedule(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun removeAll() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
